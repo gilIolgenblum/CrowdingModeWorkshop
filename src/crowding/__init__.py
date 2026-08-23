3b8198d022d488a6caa6b96eda7d18850113421d
Gil I. Olgenblum <olgenblum@gmail.com>
2026-08-23 15:59:36 +0300

    fix: export plotting tools in purely binary package

--- src/crowding/__init__.py
+++ src/crowding/__init__.py
@@ -1,28 +1,25 @@
 from .constants import Constants
 from .protein import Protein
-from .cosolute import Cosolute, CosoluteMixture
+from .cosolute import Cosolute
 from .binary import CrowdingModel
 from .statistics import monte_carlo_subsampling
+from .plotting import BinaryPlotter
 
 # Aliases for backwards compatibility with notebooks
 var = Constants
 protein = Protein
 cosolute = Cosolute
-cosolutes = CosoluteMixture
 crowding = CrowdingModel
 
 __all__ = [
     "Constants",
     "Protein",
     "Cosolute",
-    "CosoluteMixture",
     "CrowdingModel",
     "BinaryPlotter",
     "monte_carlo_subsampling",
     "var",
     "protein",
     "cosolute",
-    "cosolutes",
-    "crowding",
-    "crowding_ter"
+    "crowding"
 ]
